a8e716f312b10d60180f5c1c33ba2033cac6a6fc
Yusuf Can Bayrak <yusuf.can.bayrak@huawei.com>
2021-12-11 13:53:56 +0300

    kafka example update

--- examples/kafka/publish.py
+++ examples/kafka/publish.py
@@ -1,9 +1,13 @@
+import sys
 from kafka import KafkaProducer
 from multiprocessing import cpu_count
 
 if __name__ == "__main__":
-    producer = KafkaProducer(bootstrap_servers='kafka:9092', )
-    topic = "robot.hw"
+    producer = KafkaProducer(bootstrap_servers='kafka-broker:9092', )
+    topic = sys.argv[-1]
+    message_count = cpu_count() * 4
     for i in range(cpu_count() * 4):
         producer.send(topic, ('KAFKA_TOPIC=%s;TEST_VAR=%s' % (topic, i)).encode("UTF-8"))
         producer.flush()
+
+    print("%s messages Sent to %s" % (message_count, topic))
