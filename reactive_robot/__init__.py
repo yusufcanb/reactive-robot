00acf3961f4527c5653bcc1394de33991f3fab8c
Yusuf Can Bayrak <yusufcanbayrak@gmail.com>
2021-12-30 13:33:32 +0300

    Update __init__.py

--- reactive_robot/__init__.py
+++ reactive_robot/__init__.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 __author__ = "Yusuf Can Bayrak <yusufcanbayrak@gmail.com>"
-__version__ = "0.1.0-alpha.2"
+__version__ = "0.1.0-alpha.3"
 
 PYTHON_VERSION = f"{sys.version_info.major}.{sys.version_info.minor}"
 PKG_DIR = os.path.dirname(os.path.abspath(__file__))
