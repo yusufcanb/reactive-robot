663c21a76116c9cecded1ce292a5eef9a8b8409c
Yusuf Can Bayrak <yusuf.bayrak@enocean.com>
2022-04-11 22:43:05 +0200

    logging colored outputs

--- reactive_robot/log.py
+++ reactive_robot/log.py
@@ -1,6 +1,8 @@
+import click
 import logging
 
 from colorlog import ColoredFormatter
+from rich.logging import RichHandler
 
 
 class Log(object):
@@ -8,13 +10,16 @@ class Log(object):
     Maintain logging level
     """
 
-    format = "%(asctime)s - [%(levelname)s] - %(message)s"
+    # format = "%(asctime)s - [%(levelname)s] - %(message)s"
+    format = "%(log_color)s[%(asctime)s] - %(levelname)-s%(reset)s - %(log_color)s%(message)s%(reset)s"
 
     def __init__(self, log_name="reactive_robot", level=logging.INFO):
         self.logger = logging.getLogger(log_name)
+
         self.logger.setLevel(1)
         self.logger.propagate = False
 
+        # self.stream = RichHandler(rich_tracebacks=False, tracebacks_suppress=[click])
         self.stream = logging.StreamHandler()
         self.stream.setLevel(level)
         self.stream.setFormatter(ColoredFormatter(self.format))
