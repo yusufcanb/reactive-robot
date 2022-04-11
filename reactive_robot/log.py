ad720c144a3eb6e509400e4d361115ae7270985d
Yusuf Can Bayrak <yusuf.bayrak@enocean.com>
2022-04-11 22:45:51 +0200

    linting

--- reactive_robot/log.py
+++ reactive_robot/log.py
@@ -2,7 +2,6 @@ import click
 import logging
 
 from colorlog import ColoredFormatter
-from rich.logging import RichHandler
 
 
 class Log(object):
@@ -10,7 +9,6 @@ class Log(object):
     Maintain logging level
     """
 
-    # format = "%(asctime)s - [%(levelname)s] - %(message)s"
     format = "%(log_color)s[%(asctime)s] - %(levelname)-s%(reset)s - %(log_color)s%(message)s%(reset)s"
 
     def __init__(self, log_name="reactive_robot", level=logging.INFO):
@@ -19,7 +17,6 @@ class Log(object):
         self.logger.setLevel(1)
         self.logger.propagate = False
 
-        # self.stream = RichHandler(rich_tracebacks=False, tracebacks_suppress=[click])
         self.stream = logging.StreamHandler()
         self.stream.setLevel(level)
         self.stream.setFormatter(ColoredFormatter(self.format))
