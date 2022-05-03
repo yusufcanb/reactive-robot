ae00b958fb23c229c8c422ad1fe032fcd45ca1d6
Yusuf Can Bayrak <yusufcanbayrak@gmail.com>
2022-05-03 22:57:37 +0300

    robot library implementation

--- reactive_robot/lib.py
+++ reactive_robot/lib.py
@@ -2,21 +2,26 @@
 This module contains keywords to use it in robot scripts.
 Contains shortcuts for common operations related to the Reactive Robot like getting payload,topic etc.
 """
+import base64
 import logging
 
 from robot.api.deco import keyword
-from robot.variables import variables
+from robot.running import EXECUTION_CONTEXTS
+
+from reactive_robot.constants import EVENT_TOPIC_VARIABLE, EVENT_PAYLOAD_VARIABLE
 
 logger = logging.getLogger(__name__)
 
 
 @keyword("Get Reactive Robot Payload")
 def get_payload_as_dict() -> dict:
-    logger.debug(variables)
-    assert NotImplementedError
+    context = EXECUTION_CONTEXTS.top
+    variables = context.variables.current.store
+    return  base64.b64decode(variables.data[EVENT_PAYLOAD_VARIABLE])
 
 
 @keyword("Get Reactive Robot Topic")
 def get_topic_as_str() -> str:
-    logger.debug(variables)
-    assert NotImplementedError
+    context = EXECUTION_CONTEXTS.top
+    variables = context.variables.current.store
+    return variables.data[EVENT_TOPIC_VARIABLE]
